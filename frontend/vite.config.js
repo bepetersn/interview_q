b4f55c163911e79136c2804397d9b7fc8b249f13
Brian E. Peterson <bepetersn@gmail.com>
2025-06-18 15:28:08 -0500

    Getting ready for deployment to AWS EB / Vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+// import fs from 'fs'
+// import path from 'path'
 
-// Always read cert/key from project root
-const cert = fs.readFileSync(path.resolve(__dirname, '../example.com+5.pem'))
-const key = fs.readFileSync(path.resolve(__dirname, '../example.com+5-key.pem'))
+// // Always read cert/key from project root
+// const cert = fs.readFileSync(path.resolve(__dirname, '../example.com+5.pem'))
+// const key = fs.readFileSync(path.resolve(__dirname, '../example.com+5-key.pem'))
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key,
-      cert,
-    },
     host: '127.0.0.1',
     port: 5173,
   },
