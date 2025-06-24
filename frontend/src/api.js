b137f01007b80db2ec88aa050872ec08d3d11084
Brian E. Peterson <bepetersn@gmail.com>
2025-06-24 17:50:22 -0500

    Append API to the base_url including for deployed environments

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 import { getCookie } from './utils.js';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://127.0.0.1:8000';
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'https://127.0.0.1:8000/api/',
+    baseURL: baseURL + '/api/',
     headers: {
         'Content-Type': 'application/json',
     },
